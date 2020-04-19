a8e08227dfe9b5550a172802a916bc38bbbb9db0
Enilton Junior <enilton.a.junior@capgemini.com>
2020-04-18 23:49:12 -0300

    fix: add cors policy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { uuid, isUuid } = require('uuidv4');
-const cors = required('cors');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
